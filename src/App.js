857fbcc4440c0a157f2bc1d5abf88e9b359e5913
JuanMend <juanmend690@gmail.com>
2020-07-10 15:29:03 -0500

    firebase working

--- src/App.js
+++ src/App.js
@@ -2,35 +2,94 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import { Input } from '@material-ui/core';
+
+function getModalStyle() {
+	const top = 50;
+	const left = 50;
+
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`
+	};
+}
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3)
+	}
+}));
 
 function App() {
-	const [posts, setPosts] = useState([
-		{
-			username: 'carlosdev',
-			caption: 'Wow it works',
-			imageUrl:
-				'https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
-		},
-		{
-			username: 'carlosdev',
-			caption: 'Wow it works',
-			imageUrl:
-				'https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
-		}
-	]);
+	const classes = useStyles();
+	const [modalStyle] = useState(getModalStyle());
+	const [posts, setPosts] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
 
 	// Runs a piece of code based on a specific condition
 	useEffect(() => {
 		// Where the code Runs
 		// On Snapshot is a listener
+		// Everytime a document changes, modifies it takes a snapshot of the document and it updates it
 		db.collection('posts').onSnapshot((snapshot) => {
 			// every time a new post is added, this code fires
-			snapshot.docs.map((doc) => doc.data());
+			setPosts(
+				snapshot.docs.map((doc) => ({
+					id: doc.id,
+					post: doc.data()
+				}))
+			);
 		});
 	}, []); // If it's empty its going to run once the page loads and it's not going to run again
 
+	const signUp = (e) => {};
+
 	return (
 		<div className="app">
+			<Modal open={open} onClose={() => setOpen(false)}>
+				<div style={modalStyle} className={classes.paper}>
+					<form className="app__signup">
+						<center>
+							<img
+								className="app__headerImage"
+								src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+								alt="logo"
+							/>
+						</center>
+						<Input
+							placeholder="username"
+							type="text"
+							value={username}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<Input
+							placeholder="email"
+							type="text"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<Input
+							placeholder="password"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<Button onClick={signUp}>Sign Up</Button>
+					</form>
+				</div>
+			</Modal>
 			<div className="app__header">
 				<img
 					className="app__headerImage"
@@ -38,9 +97,14 @@ function App() {
 					alt="logo"
 				/>
 			</div>
+			<Button onClick={() => setOpen(true)}>Sign Up</Button>
 
-			{posts.map((val) => {
-				<Post username={val.username} caption={val.caption} imageUrl={val.imageUrl} />;
+			{posts.map(({ id, post }) => {
+				return (
+					<div>
+						<Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+					</div>
+				);
 			})}
 		</div>
 	);
