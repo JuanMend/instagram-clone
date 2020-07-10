25327cd256a5626c27bc6386477c7500c2b06954
JuanMend <juanmend690@gmail.com>
2020-07-10 14:27:32 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
+import { db } from './firebase';
 
 function App() {
-	const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState([
+		{
+			username: 'carlosdev',
+			caption: 'Wow it works',
+			imageUrl:
+				'https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+		},
+		{
+			username: 'carlosdev',
+			caption: 'Wow it works',
+			imageUrl:
+				'https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+		}
+	]);
+
+	// Runs a piece of code based on a specific condition
+	useEffect(() => {
+		// Where the code Runs
+		// On Snapshot is a listener
+		db.collection('posts').onSnapshot((snapshot) => {
+			// every time a new post is added, this code fires
+			snapshot.docs.map((doc) => doc.data());
+		});
+	}, []); // If it's empty its going to run once the page loads and it's not going to run again
+
 	return (
 		<div className="app">
 			<div className="app__header">
@@ -13,21 +38,10 @@ function App() {
 					alt="logo"
 				/>
 			</div>
-			<Post
-				username="carlosdev"
-				caption="Wow it works"
-				imageUrl="https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-			/>
-			<Post
-				username="daveLink"
-				caption="VRChat is Awesome"
-				imageUrl="https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-			/>
-			<Post
-				username="Mario"
-				caption="Fun Project"
-				imageUrl="https://images.unsplash.com/photo-1593874859865-ba989722a38d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-			/>
+
+			{posts.map((val) => {
+				<Post username={val.username} caption={val.caption} imageUrl={val.imageUrl} />;
+			})}
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+	apiKey: 'AIzaSyAWu5kLFh8rpepjOrrDXvG8nA9I5v6SZGM',
+	authDomain: 'instagram-clone-e7777.firebaseapp.com',
+	databaseURL: 'https://instagram-clone-e7777.firebaseio.com',
+	projectId: 'instagram-clone-e7777',
+	storageBucket: 'instagram-clone-e7777.appspot.com',
+	messagingSenderId: '39322115611',
+	appId: '1:39322115611:web:649a95dae920fee8cf62be',
+	measurementId: 'G-XKNGX2NZ2T'
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
